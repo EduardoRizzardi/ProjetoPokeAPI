ff1dc8f8ea6046b8c703529457a0833d62bebb70
Eduardo Cappellari Rizzardi <182200153@senacinformatica.br>
2024-10-22 19:53:32 -0300

    Adicionando cor de fundo aos cards

--- src/js/card/card.js
+++ src/js/card/card.js
@@ -1,25 +1,23 @@
-import {pokemonList} from "../constants/constants.js"
-
-//Ta carregando somente 20 pokemon pq se for carragar todos ira demorar(Fazer função que carrega aos poucos)
-
+import { pokemonList } from "../constants/constants.js";
 
 export function createCard(pokemon, index, type, type2) {
     console.log(pokemon);
-    
+
     const typeimg1 = `src/assets/types/${type}.svg`;
     const typeimg2 = type2 ? `src/assets/types/${type2}.svg` : '';  
 
-    const card = `<div class="card" style="width: 18rem;">
+    // Adicionando classes de tipo ao card
+    const card = `<div class="card ${type} ${type2 ? type2 : ''}" style="width: 18rem;">
                     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <img src="${typeimg1}" alt="${type} type image"> <img src="${typeimg2}"> 
+                        <img src="${typeimg1}" alt="${type} type image"> 
+                        ${type2 ? `<img src="${typeimg2}" alt="${type2} type image">` : ''} 
                         <h5 class="card-title">${pokemon.name}</h5>
                         <p class="text-id">N° ${index}</p>
-                        <div id="type-image-container"></div>
-                        <p class="text-type">Type: ${type} ${type2}</p>
+                        <p class="text-type">Type: ${type} ${type2 ? `/ ${type2}` : ''}</p>
                         <a href="${pokemon.url}" class="btn btn-primary">Ver mais</a>
                     </div>
-                </div>`
+                </div>`;
 
     pokemonList.innerHTML += card;
 }
