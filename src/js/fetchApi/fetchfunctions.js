2fa71ff7abc248ef5a3f076ca4e1130030bfc5c7
Douglasx06 <douglassilvaalves336@gmail.com>
2024-10-24 14:19:19 -0300

    arrumei algumas linhas de codigo e add uma animação nos cards

--- src/js/fetchApi/fetchfunctions.js
+++ src/js/fetchApi/fetchfunctions.js
@@ -1,23 +1,6 @@
 import {urlPokeApi} from "../constants/constants.js"
 import showError from "../errors/errors.js";
 
-const colors = {
-    fire: '#FDDFDF',
-    grass: '#DEFDE0',
-    electric: '#FCF7DE',
-    water: '#DEF3FD',
-    ground: '#f4e7da',
-    rock: '#d5d5d4',
-    fairy: '#fceaff',
-    poison: '#98d7a5',
-    bug: '#f8d5a3',
-    dragon: '#97b3e6',
-    psychic: '#eaeda1',
-    flying: '##A9A9A9',
-    fighting: '#E6E0D4',
-    normal: '#F5F5F5',
-    ice: '#98d8d8'
-}
 
 export async function listAllPokemons(urlApi = urlPokeApi){
     try{
@@ -68,7 +51,4 @@ export async function listTypePoke2(pokemon) {
 }
 
 export async function imgType(type, type2) {    
-    const color = colors[type];
-
-    console.log(colortype);
 }
